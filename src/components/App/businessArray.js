f5e3e03bca60ab757ad97dca66a2f5e26aee63bd
Nicola Rivosecchi <93051750+nicolaRivo@users.noreply.github.com>
2023-07-09 12:19:29 +0200

    Add files via upload

---
+++ src/components/App/businessArray.js
@@ -0,0 +1,26 @@
+const businessArray =[
+    {
+    imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+    name: 'MarginOtto Pizzeria',
+    address: '1010 Paddington Way',
+    city: 'Flavortown',
+    state: 'NY',
+    zipCode: '10101',
+    category: 'Italian',
+    rating: 4.5,
+    reviewCount: 90
+    },
+    {
+    imageSrc: 'https://media-cdn.tripadvisor.com/media/photo-s/03/86/83/06/pizzeria-calafuria.jpg',
+    name: 'Calafuria',
+    address: 'Viale Marche, 65',
+    city: 'Milan',
+    state: 'MI',
+    zipCode: '20159',
+    category: 'Italian',
+    rating: 3.5,
+    reviewCount: 20
+    }
+]
+
+export default businessArray;
